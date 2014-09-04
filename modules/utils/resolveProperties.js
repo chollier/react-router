1558df7be2998da8fb2365dcbc78c964683e13aa
Michael Jackson <mjijackson@gmail.com>
2014-09-04 07:59:49 -0700

    [added] didTransitionTo, didTransitionFrom and getAsyncProps hooks [changed] willTransitionFrom is synchronous [removed] AsyncState mixin
    Fixes #261

---
+++ modules/utils/resolveProperties.js
@@ -0,0 +1,17 @@
+var Promise = require('when/lib/Promise');
+
+function resolveProperties(object) {
+  var keys = Object.keys(object);
+
+  return Promise.all(keys.map(function (key) {
+    return object[key];
+  })).then(function (values) {
+    keys.forEach(function (key, index) {
+      object[key] = values[index];
+    });
+
+    return object;
+  });
+}
+
+module.exports = resolveProperties;
